d654e68d758d941ff2cc2872a636916bfaa63d41
EliTheDev <elithedev@gmail.com>
2018-09-10 14:34:19 -0700

    Added img change scripting

---
+++ mapExample/mapExample/www/scripts/imgChange.js
@@ -0,0 +1,8 @@
+﻿function chngSrc(mod) {
+    src = document.getElementById("panImg");
+    if (mod == "bath") {
+        src = "images/mapBath.png";
+    } else if (mod == "reg") {
+        src = "images/map.png";
+    }
+}
--- mapExample/mapExample/www/scripts/jquerystuff.js
+++
@@ -1,6 +0,0 @@
-// JavaScript source code
-define(["jquery", "plugins/jquery.panzoom"], function ($) {
-    $(document).ready(function () {
-        $(".panzoom").panzoom();
-    });
-});
